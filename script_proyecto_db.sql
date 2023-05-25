7c861fe5d3811cc17115b4e6cc450cbfa79f2dc9
JaimeOmar1904 <jaimemarcial@comunidad.unam.mx>
2023-05-25 14:21:15 -0600

    Ejercicio 2

--- script_proyecto_db.sql
+++ script_proyecto_db.sql
@@ -15,3 +15,23 @@ grant select on credito.* to 'usuario2'@'localhost';
 grant insert on credito.* to 'usuario2'@'localhost';
 ALTER USER 'operador1'@'localhost' WITH MAX_USER_CONNECTIONS 3;
 
+CREATE PROCEDURE calcular_consumo_diario
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    INSERT INTO concentrador_consumo(fecha, nombre_tienda, tipo_movimiento, total_consumo_cancelacion)
+    SELECT 
+        CONVERT(date, GETDATE()), 
+        t.nombre, 
+        c.tipo_movimiento, 
+        SUM(c.cantidad) 
+    FROM 
+        tienda t 
+        INNER JOIN consumo c ON t.id_tienda = c.id_tienda 
+    WHERE 
+        CONVERT(date, c.fecha) = CONVERT(date, GETDATE()) 
+    GROUP BY 
+        t.nombre, 
+        c.tipo_movimiento;
+END
