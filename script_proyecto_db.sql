01fc9c24fa7ec35d245ec0caa4aa348f5c57656a
alexis <alexisjesuscordovadelacruz@gmail.com>
2023-05-25 14:11:35 -0600

    Correccion punto 1

--- script_proyecto_db.sql
+++ script_proyecto_db.sql
@@ -1,4 +1,5 @@
 use Credito;
+
 -- Crear dos usuario para la base Crédito
 create user 'usuario1'@'localhost' identified by '123456%';
 create user 'usuario2'@'localhost' identified by '12345%';
@@ -10,7 +11,7 @@ ALTER USER 'usuario1'@'localhost' WITH MAX_QUERIES_PER_HOUR 20;
 
 -- El otro usuario escribir y leer en todas las tablas de la base y no puede tener
 -- mas de 3 conexiones concurrentes ni hacer ningún movimiento DDL
-grant update on credito.* to 'usuario2'@'localhost';
 grant select on credito.* to 'usuario2'@'localhost';
-grant delete on credito.* to 'usuario2'@'localhost';
-ALTER USER 'operador1'@'localhost' FAILED_LOGIN_ATTEMPTS 3 PASSWORD_LOCK_TIME 3;
+grant insert on credito.* to 'usuario2'@'localhost';
+ALTER USER 'operador1'@'localhost' WITH MAX_USER_CONNECTIONS 3;
+
